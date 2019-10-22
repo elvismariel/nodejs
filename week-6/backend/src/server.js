562b7fef974b8c521a9bdb73a55c9b2caa47e2ff
elvismariel <elvismariel@gmail.com>
2019-10-22 17:20:40 -0300

    Conclusao do box

--- week-6/backend/src/server.js
+++ week-6/backend/src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+    console.log("ok");
+});
 
 mongoose.connect('mongodb://localhost/dropbox', {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -14,5 +22,6 @@ mongoose.connect('mongodb://localhost/dropbox', {
 app.use(express.json());    // Define que vai trabalhar com Json
 app.use(express.urlencoded({extended: true }));  // Define que vai ser usado arquivos de upload
 app.use(require('./routes')); // Define a importacao do arquivo de rotas
+app.use("/files", express.static(path.resolve(__dirname, "..","tmp"))); // Libera acesso aos arquivos staticos da pasta tmp
 
-app.listen(3333);
+server.listen(3333);
